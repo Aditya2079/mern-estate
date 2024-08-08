e3e9460a219d133a42aaa24fd3077cc1cfba15b0
Aditya2079 <adityadkadam2079@gmail.com>
2024-08-08 13:21:09 +0530

    connect to database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,15 @@ import Home from "./home"
 import Signin from "./signin"
 import Signup from "./signup"
 import Profile from "./profile"
+import Header from './Header'
 
 export default function App() {
   return (
     
    < BrowserRouter>
+    <Header />
    <Routes>
-  <Route path="/" element={<Home />}></Route>
+    <Route path="/" element={<Home />}></Route>
   <Route path="/signin" element={<Signin />}></Route>
   <Route path="/signup" element={<Signup />}></Route>
   <Route path="/profile" element={<Profile />}></Route>
