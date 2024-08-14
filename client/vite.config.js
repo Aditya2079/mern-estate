c74fa681128d684b94535449ad73759dfbe654c2
Aditya2079 <adityadkadam2079@gmail.com>
2024-08-14 17:47:32 +0530

    complete signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true, // Add this line for handling CORS correctly
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()], // Moved this to the correct location
 })
