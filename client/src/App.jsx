7da841b36d34cedd8b3f26a84c322e2a942448b1
Aditya2079 <adityadkadam2079@gmail.com>
2024-08-14 17:31:01 +0530

    complete signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { Profiler } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./home"
-import Signin from "./signin"
-import Signup from "./signup"
-import Profile from "./profile"
-import Header from './Header'
+import Home from "./pages/home"
+import Signin from "./pages/signin"
+import Signup from "./pages/signup"
+import Profile from "./pages/profile"
+import Header from './components/Header'
 
 export default function App() {
   return (
